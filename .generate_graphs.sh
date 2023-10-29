fbf9a78ba303dbf8e2e5efa075fe75ed6100e89a
IggyTheWolf <iggy@iggycorp.com>
2023-10-29 10:37:25 -0700

    Autocommit from 2023-10-29 10:37:25

--- .generate_graphs.sh
+++ .generate_graphs.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /home/pi/klipper_config/shaper_calibrate_x.png
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /home/pi/klipper_config/shaper_calibrate_y.png
+~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /tmp/shaper_calibrate_x.png
+~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /tmp/shaper_calibrate_y.png
--- .graph_resonance.sh
+++ .graph_resonance.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/bash
 
-~/klipper/scripts/graph_accelerometer.py -c /tmp/raw_data_axis*.csv -o /home/pi/klipper_config/resonances.png
+~/klipper/scripts/graph_accelerometer.py -c /tmp/raw_data_axis*.csv -o /tmp/resonances.png
+
